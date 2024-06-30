2fd318156d838cbade25246d8acddedf5aabb9b6
neonnghost <iamneonghost@gmail.com>
2024-06-30 10:59:10 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -150,7 +150,7 @@ async def start_command(client: Client, message: Message):
                 link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API,f'https://telegram.dog/{client.username}?start=verify_{token}')
                 btn = [
                     [InlineKeyboardButton("Click here", url=link)],
-                    [InlineKeyboardButton('How to use the bot', url=TUT_VID)]
+                    [InlineKeyboardButton('Video Tutorial', url=TUT_VID)]
                 ]
                 await message.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 16 Hour after passing the ad. don,t know how to verify click on video tutorial ", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
 
